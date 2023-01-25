9bf7e9184bd9396c1f7c8f6eed486b6198bbb82f
Aryan Prince <aryanprincesanty@icloud.com>
2023-01-25 04:42:33 +0000

    Updated to HTTPS API calls now

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const [allMovies, setAllMovies] = useState<MovieType[]>([])
   const [searchTerm, setSearchTerm] = useState('')
 
-  const API_URL = `http://www.omdbapi.com?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}`
+  const API_URL = `https://www.omdbapi.com?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}`
 
   const searchMovies = async (title: string) => {
     const response = await fetch(`${API_URL}&s=${title}`)
